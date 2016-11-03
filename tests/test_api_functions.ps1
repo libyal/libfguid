50137d14c0415b00aa03c86761b30789d7e143a2
Joachim Metz <joachim.metz@gmail.com>
2016-11-03 20:01:23 +0100

    Worked on tests

--- tests/test_api_functions.ps1
+++ tests/test_api_functions.ps1
@@ -10,7 +10,7 @@ $TestPrefix = Split-Path -path ${Pwd}.Path -parent
 $TestPrefix = Split-Path -path ${TestPrefix} -leaf
 $TestPrefix = ${TestPrefix}.Substring(3)
 
-$TestFunctions = "get_version error identifier_copy_from identifier_to_string" -split " "
+$TestFunctions = "error identifier_copy_from identifier_to_string support" -split " "
 
 $TestToolDirectory = "..\vs2010\Release"
 
